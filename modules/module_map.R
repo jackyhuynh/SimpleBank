a13f02a7b1f4d225f4923cb54efc385ad0297eaf
Truc Huynh <jackyhuynh87@gmail.com>
2021-03-16 19:10:51 -0400

    Update module_map.R

--- modules/module_map.R
+++ modules/module_map.R
@@ -28,19 +28,43 @@ UserTransaction <-
   aggregate(select(transactions[transactions$type == 'debit', ], -type)['Amount'], by =
               select(transactions[transactions$type == 'debit', ], -type)['category'], sum)
 
+# Sort the User Transaction
+UserTransaction <- UserTransaction[order(-UserTransaction[,2]),]
+
+UserTransaction$TransPercent <- scales::percent(as.numeric(UserTransaction$Amount/sum(UserTransaction$Amount)))
 
 
-pie3D(
-  UserTransaction$Amount,
-  labels = UserTransaction$category,
-  height = 0.05,
-  radius = 1.5,
-  explode = 0.3,
-  shade = 0.7,
-  broder= 'white',
-  col = heat.colors(length(UserTransaction$Amount))
+UserBigTransaction <- filter(UserTransaction, UserTransaction$TransPercent>1)
+
+c("other expense",
+  sum(UserTransaction$Amount)-sum(UserBigTransaction$Amount),
+  (sum(UserTransaction$Amount)-sum(UserBigTransaction$Amount))/sum(UserTransaction$Amount))
+
+
+par(mar = c(1, 1, 1, 1)) # bltr
+
+pie(
+  Type, 
+  edges = 200, 
+  radius = 0.8,
+  clockwise = TRUE, # IMPORTANT
+  angle = 45, 
+  col = viridis::viridis_pal(option = "magma", direction=-1)(length(UserTransaction$Amount)),  # BETTER COLOR PALETTE
+  labels = tail(piepercent, -7), # NEVER DISPLAY OVERLAPPING LABELS
+  cex = 0.7
 )
 
-# legend("topright", UserTransaction$category, 
-#        cex = 0.7, fill = heat.colors(length(UserTransaction$Amount)))
+legend(
+  x = 1.2, # DELIBERATE POSITION
+  y = 0.5, # DELIBERATE POSITION
+  inset = .05, 
+  title = "Primary Crime Type", 
+  legend = names(Type), # YOU WERE PASSING IN _ALL_ THE REPEAT NAMES
+  fill = viridis::viridis_pal(option = "magma", direction=-1)(length(Type)),  # USE THE SAME COLOR PALETTE
+  horiz = FALSE,
+  cex = 0.6, # PROPER PARAMETER FOR TEXT SIZE
+  text.width = 0.7 # SET THE BOX WIDTH
+)
+
+
 
