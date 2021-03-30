48b5dcbecf7b591153404a89bad06851dc1d8fd4
SwethaGidugu <swethaganeshv@gmail.com>
2021-03-30 13:11:01 -0400

    Added insert query for users table

--- SQLFiles/CreateDatabaseAndTableFile.sql
+++ SQLFiles/CreateDatabaseAndTableFile.sql
@@ -84,3 +84,5 @@ CREATE TABLE `users` (
   UNIQUE KEY `ID_UNIQUE` (`ID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
+Insert into users (ID,USERID,PASSWORD)
+			VALUES(4,'user4','pass4');
