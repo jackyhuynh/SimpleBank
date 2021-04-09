8310541546d62d07e93bca29789dea5ae94f00e1
Truc Huynh <jackyhuynh87@gmail.com>
2021-04-09 09:41:33 -0400

    Update UITester.R

--- App_V.02/Test/UITester.R
+++ App_V.02/Test/UITester.R
@@ -10,55 +10,74 @@
 library(shiny)
 
 ## Only run examples in interactive R sessions
-if (interactive()) {
-    
-    ui <- fluidPage(
-        sidebarLayout(
-            sidebarPanel(
-                fileInput("file1", "Choose CSV File",
-                          accept = c(
-                              "text/csv",
-                              "text/comma-separated-values,text/plain",
-                              ".csv")
-                ),
-                tags$hr(),
-                checkboxInput("header", "Header", TRUE),
-                
-                # Button
-                downloadButton("downloadData", "Download")
-            ),
-            mainPanel(
-                tableOutput("contents")
-            )
+###Get the connection from the database
+connection <- getConnection();
+# Get the location list
+rs <-
+    dbSendQuery(
+        connection,
+        paste0(
+            "select * from user_details where user_id='",
+            value ,"'"
         )
     )
-    
-    server <- function(input, output) {
-        output$contents <- renderTable({
-            # input$file1 will be NULL initially. After the user selects
-            # and uploads a file, it will be a data frame with 'name',
-            # 'size', 'type', and 'datapath' columns. The 'datapath'
-            # column will contain the local filenames where the data can
-            # be found.
-            inFile <- input$file1
-            
-            if (is.null(inFile))
-                return(NULL)
-            
-            read.csv(inFile$datapath, header = input$header)
-        })
-        
 
-        
-        output$downloadData <- downloadHandler(
-            filename = function() {
-                paste(input$file1, ".csv", sep = "")
-            },
-            content = function(file) {
-                write.csv(contents(), file, row.names = FALSE)
-            }
+currentUser <- dbFetch(rs)
+
+dbDisconnect(connection)
+
+#Label mandatory fields
+labelMandatory <- function(label) {
+    tagList(label,span("*", class = "mandatory_star")
+    )}
+
+ui <- fluidPage(
+    tags$div(
+    sidebarLayout(
+        sidebarPanel(),
+        mainPanel(
+            id="User Information",
+            
+            tags$h2("Personal Information"),br(),
+            tags$div(
+                splitLayout(
+                    cellWidths = c("200px","20", "250px"),
+                    cellArgs = list(style = "vertical-align: top"),
+                    textInput(
+                        "fullName",labelMandatory("Full Name"), placeholder = currentUser[[2]],width = '200px'),
+                    tags$div(),
+                    textInput(
+                        "address","Address", placeholder = currentUser[[3]],width = '200px')
+                )
+            ),
+            tags$div(
+                splitLayout(
+                    cellWidths = c("200px","20", "250px"),
+                    cellArgs = list(style = "vertical-align: top"),
+                    textInput(
+                        "fullName","Birthday", placeholder = currentUser[[5]],width = '200px'),
+                    tags$div(),
+                    textInput(
+                        "address","SSN", placeholder = '*********',width = '200px')
+                )
+            ),
+            tags$h2("Account Information"),br(),
+            tags$div(
+                splitLayout(
+                    cellWidths = c("200px","20", "250px"),
+                    cellArgs = list(style = "vertical-align: top"),
+                    textInput(
+                        "fullName",labelMandatory("User Name"), placeholder = currentUser[[6]],width = '200px'),
+                    tags$div(),
+                    textInput(
+                        "address",labelMandatory("Password Name"), placeholder = currentUser[[7]],width = '200px')
+                )
+            ),
         )
-    }
+    )
+)
+)
     
-    shinyApp(ui, server)
-}
+server <- function(input, output) {}
+    
+shinyApp(ui, server)
