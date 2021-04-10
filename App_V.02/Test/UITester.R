b3be5088c2f986a217d0377c76af80974f8cf74d
Truc Huynh <jackyhuynh87@gmail.com>
2021-04-10 14:16:16 -0400

    Update UITester.R

--- App_V.02/Test/UITester.R
+++ App_V.02/Test/UITester.R
@@ -1,6 +1,40 @@
 library(shiny)
 
-ui<-fluidPage()
+connection <- getConnection()
+UserTransaction<-getTransactionDataWithStoreName(connection)
+
+ui<-fluidPage(
+  sidebarLayout(
+    sidebarPanel(
+      # Monitor and control the action of all Transactions Panel
+      conditionalPanel(
+        'input.dataset === "All Transaction"',
+        checkboxGroupInput(
+          "show_trans2",
+          "Selections:",
+          names(UserTransaction[, c("Date", "Time", "Type", "Amount", "Category", "accounts")]),
+          selected = names(UserTransaction[, c("date", "description", "amount", "type", "category", "accounts")])
+        )
+      )
+    ),
+    mainPanel(
+      #All Transaction
+      tabPanel(
+        "All Transaction",
+        DT::dataTableOutput("transTable"),
+        
+        printWhiteSpace(),
+        tags$em(tags$h3("Deep Analyzing", class = "text-primary")),
+        
+        tabsetPanel(
+          tabPanel("Map Expense",leafletOutput("map")),
+          tabPanel("Total Income")
+        ) # End tabset Panel
+        
+      )
+    )
+  )
+)
 
 server<-function(input,output, session){
     
