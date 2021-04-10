e8fbfb27c7b50a140243f1c71083a4f9d52591be
Truc Huynh <jackyhuynh87@gmail.com>
2021-04-10 14:01:20 -0400

    Update FunctionTester.R

--- App_V.02/Test/FunctionTester.R
+++ App_V.02/Test/FunctionTester.R
@@ -5,9 +5,11 @@ dbDisconnect(connection)
 
 
 ##1. Get data for all the transactions with store name
-transactions <-getTransactionDataWithStoreName(connection)
+getTransactionDataWithStoreName(connection)
 
 ##9. getCategoryNames/ Get all the category
 categoriesList <- getCategoryList(connection)
 View(categoriesList)
 
+##2. get USer ID based on username and password
+getUserID('inewton', 'pwd123', connection)
