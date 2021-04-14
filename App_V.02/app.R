3e8afb5cc2db64154380e2c66e6091ade69d4a95
Truc Huynh <jackyhuynh87@gmail.com>
2021-04-14 09:58:05 -0400

    Update app.R

--- App_V.02/app.R
+++ App_V.02/app.R
@@ -24,6 +24,7 @@ library(leaflet) # map
 source("modules/charts_modules.R")
 source("modules/print_display_module.R")
 source("modules/user_checking_accounts.R")
+source("modules/login_modules.R")
 
 # @Wrucha
 # Source for setup and communicate with database system
@@ -32,66 +33,6 @@ source("modules/DataClasses.R")
 source("modules/TransactionFunctions.R")
 source("modules/UserFunctions.R")
 source("modules/TransacationCategoryFunction.R")
-
-# @Swetha
-#
-# Name: Main login screen
-# Function: UI Component for Login Screen
-# Component: UI
-# Variable: Global
-loginpage <-
-  div(
-    id = "loginpage",
-    style = "width: 500px; max-width: 100%; margin: 0 auto; padding: 20px;",
-    wellPanel(
-      tags$h2("LOG IN", class = "text-center", style = "padding-top: 0;color:#333; font-weight:600;"),
-      textInput(
-        "userName",
-        placeholder = "Username",
-        label = tagList(icon("user"), "Username")
-      ),
-      passwordInput(
-        "passwd",
-        placeholder = "Password",
-        label = tagList(icon("unlock-alt"), "Password")
-      ),
-      br(),
-      div(
-        style = "text-align: center;",
-        actionButton(
-          "login",
-          "SIGN IN",
-          style = "color: white; background-color:#3c8dbc;
-                                 padding: 10px 15px; width: 150px; cursor: pointer;
-                                 font-size: 18px; font-weight: 600;"
-        ),
-        shinyjs::hidden(div(
-          id = "nomatch",
-          tags$p(
-            "Incorrect username or password!",
-            style = "color: red; font-weight: 600;
-                                            padding-top: 5px;font-size:16px;",
-            class = "text-center"
-          )
-        )),
-        br()
-      )
-    )
-  )
-# End Main Login UI
-
-
-#  @Swetha
-# 
-# Function: Ignore lines 64-68 as they contain static user data
-# Component: UI
-# Variable: Global
-credentials = data.frame(
-  username_id = c("myuser", "myuser1"),
-  passod   = sapply(c("mypass", "mypass1"), password_store),
-  permission  = c("basic", "advanced"),
-  stringsAsFactors = F
-)
 
 
 # @Truc
@@ -229,7 +170,7 @@ server <- function(input, output, session) {
   # End output$sidebarpanel
   
   
-  # @Truc
+  # @Truc @Swetha
   #
   # Function: UI Component for the welcome page
   # Component: UI, Hold majority part of user Welcome Page UI
