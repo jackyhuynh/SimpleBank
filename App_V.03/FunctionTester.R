ba429d40d6b3a35b1e1bdc1ad4b09eaae18d7031
Truc Huynh <jackyhuynh87@gmail.com>
2021-04-15 13:07:49 -0400

    Update FunctionTester.R

--- App_V.03/FunctionTester.R
+++ App_V.03/FunctionTester.R
@@ -27,10 +27,31 @@ df<-data.frame(Type,
 ggplot(df, aes(y=Type, x=Amount, fill=Type)) + 
   geom_bar(stat='identity',position = "stack") +
   ggplot2::theme(axis.text.x = ggplot2::element_text(angle = 60, hjust = 1))
+#########################################
+connection<-getConnection()
+user_id=1
 
+query1<-sprintf("SELECT sum(amount) FROM credit_card_analysis2.user_transaction_user_id_%s;",user_id)
+rs <- dbSendQuery(connection, query1)
+Expense<-dbFetch(rs)
+dbClearResult(rs)
 
+query2<-sprintf("SELECT date_of_transaction FROM credit_card_analysis2.user_transaction_user_id_%s;",user_id)
+rs <- dbSendQuery(connection, query2)
+DateBetween<-dbFetch(rs)
+dbClearResult(rs)
+DateBetween$date_of_transaction <- as.Date(DateBetween$date_of_transaction,format = "%Y-%m-%d")
+MonthBetween<- interval(min(DateBetween$date_of_transaction),max(DateBetween$date_of_transaction))%/% months(1)
 
+query3<-sprintf("SELECT income FROM credit_card_analysis2.user_details where user_id=%s;",user_id)
+rs <- dbSendQuery(connection, query3)
+Income<-dbFetch(rs)
+dbClearResult(rs)
 
+Income$income <- Income$income*MonthBetween
+df <- cbind(Income,Expense)
+df<- c('Total Income','Total Expense')
+dbDisconnect(connection)
 
 # ANALYZE BY CARDS
 # Testing purpose only
