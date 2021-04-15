d84236536f01e2ddd7108dcf9541aae7bd5fd6ae
Truc Huynh <jackyhuynh87@gmail.com>
2021-04-15 09:23:10 -0400

    Create FunctionTester.R

---
+++ App_V.03/FunctionTester.R
@@ -0,0 +1,37 @@
+##9. getCategoryNames/ Get all the category
+categoriesList <- getCategoryList(connection)
+View(categoriesList)
+
+
+connection<-getConnection()
+
+UserTransaction<-getTransactionDataWithStoreName(connection,1)
+
+
+
+
+##SET OF DEBIT
+debitTrans <- filter(UserTransaction,'Type'=="Debit")
+
+AllTransaction <- aggregate(Amount~Category+Type, data=UserTransaction, FUN=sum)
+
+AllTransaction$Type <- as.character(AllTransaction$Type)
+
+debitTrans <- filter(AllTransaction,AllTransaction$Type=="Debit")
+
+
+date_1 <-min(UserTransaction$Date)           # Create example dates
+date_2 <- max(UserTransaction$Date)
+install.packages("lubridate")             # Install lubridate package
+library("lubridate")                      # Load lubridate package
+interval(date_1, date_2) %/% months(1)    # Apply interval & months
+# 52
+
+
+# ANALYZE BY CARDS
+# Testing purpose only
+# This is the final query requirement 
+connection<-getConnection()
+UserCategory<-aggregate(amount~accounts, UserTransaction, FUN=sum)
+getTransactions2(1,connection)
+
