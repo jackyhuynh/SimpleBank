dba908284ed6d2be7ce54329e7ef67df012b2ed2
Truc Huynh <jackyhuynh87@gmail.com>
2021-04-16 12:42:11 -0400

    Create credit_card_analysis2_user_details.sql

---
+++ App_V.03/data/credit_card_analysis2_user_details.sql
@@ -0,0 +1,58 @@
+-- MySQL dump 10.13  Distrib 8.0.23, for Win64 (x86_64)
+--
+-- Host: localhost    Database: credit_card_analysis2
+-- ------------------------------------------------------
+-- Server version	8.0.23
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `user_details`
+--
+
+DROP TABLE IF EXISTS `user_details`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `user_details` (
+  `user_id` int NOT NULL AUTO_INCREMENT,
+  `name_on_card` varchar(50) DEFAULT NULL,
+  `address` varchar(200) DEFAULT NULL,
+  `user_ssn` varchar(20) DEFAULT NULL,
+  `date_of_birth` date DEFAULT NULL,
+  `login_username` varchar(20) DEFAULT NULL,
+  `login_password` varchar(30) DEFAULT NULL,
+  `deleted` int DEFAULT '1',
+  `income` double DEFAULT NULL,
+  PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user_details`
+--
+
+LOCK TABLES `user_details` WRITE;
+/*!40000 ALTER TABLE `user_details` DISABLE KEYS */;
+INSERT INTO `user_details` VALUES (1,'Miles Davis','123 E Fort Wayne Street, Warsaw, IN','1234567890','1984-01-16','davis_miles','password123',1,30000),(2,'Stan Wright','56 N Colosseum Blvd, Fort Wayne, IN','8899776644','1963-11-07','stanwright','password123',1,4000),(3,'Hugh J','451 Walter Way, New York, NY','3344665500','1977-05-05','jhugh','password123',1,2500),(4,'Jane Doe','78 Apartment D, Sunset Street, Detroit, MI','5012369874','1993-03-16','doe-jane','password123',1,10000);
+/*!40000 ALTER TABLE `user_details` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-04-16 12:40:10
