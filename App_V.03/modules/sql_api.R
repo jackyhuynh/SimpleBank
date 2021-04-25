59796b2a1e6f72014d83c99c8e800ce057047caa
Truc Huynh <jackyhuynh87@gmail.com>
2021-04-25 10:33:28 -0400

    Update sql_api.R

--- App_V.03/modules/sql_api.R
+++ App_V.03/modules/sql_api.R
@@ -278,9 +278,12 @@ checkUserIDExist <- function(username, connection){
 UpdateCategoryForTransaction <- function(user_id, categoryId, transactionId,connections){
   
   ##Update user_details
-  query <- sprintf("update user_transaction_user_id_%s set category_id_fk = %s where transaction_id = %s and deleted=1", user_id, categoryId, transactionId)
+  query <- sprintf("update user_transaction_user_id_%s set category_id_fk = %s where transaction_id = %s and deleted=1",
+                   user_id, categoryId, transactionId)
+  
   rs <- dbSendQuery(connection, query)
   value <- dbHasCompleted(rs)
+  
   print(paste("Log: User record updated success:",value ))
   dbClearResult(rs)
   dbDisconnect(connection) 
